c5a630be841f5317ee77e47cb1916c87f1f66579
DeelanLasrado <deelanlasrado44@gmail.com>
2023-11-29 15:56:55 +0530

    Level 1 Authentication

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express=require("express");
 const bodyParser=require("body-parser");
 const ejs=require("ejs");
+const mongoose=require("mongoose");
 
 const app=express();
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+const userSchema={
+    email:String,
+    password:String
+};
+
+const User=new mongoose.model("User",userSchema);
+
+
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -21,7 +32,44 @@ app.get("/login",(req,res)=>{
     res.render("login.ejs");
 })
 
+app.post("/register",(req,res)=>{
+    const newUser = new User ({
+        email : req.body.username,
+        password:req.body.password
+    });
 
+    newUser.save()
+        .then(() => {
+            res.render("secrets.ejs");
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+
+});
+
+app.post("/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({ email: username }).exec()
+        .then((findUser) => {
+            if (findUser) {
+                // Assuming you have a 'password' field in your userSchema
+                if (findUser.password === password) {
+                    res.render("secrets.ejs");
+                } else {
+                    res.send("Incorrect password");
+                }
+            } else {
+                res.send("User not found");
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send("Internal Server Error");
+        });
+});
 
 
 app.listen(3000,function(){
