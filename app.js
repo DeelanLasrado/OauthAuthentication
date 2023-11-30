c87f254feb396046bdb892d2207a35c9499bd7a4
DeelanLasrado <deelanlasrado44@gmail.com>
2023-11-30 06:57:37 +0530

    Level 4- Salting and Hashing Passwords with bcrypt

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express=require("express");
 const bodyParser=require("body-parser");
 const ejs=require("ejs");
 const mongoose=require("mongoose");
-const md5=require("md5");
+const bcrypt=require("bcrypt");
+const saltRounds=10;
 
 const app=express();
 mongoose.connect("mongodb://localhost:27017/userDB");
@@ -37,34 +38,39 @@ app.get("/login",(req,res)=>{
 })
 
 app.post("/register",(req,res)=>{
-    const newUser = new User ({
-        email : req.body.username,
-        password:md5(req.body.password)
-    });
-
-    newUser.save()
-        .then(() => {
-            res.render("secrets.ejs");
-        })
-        .catch((error) => {
-            console.error(error);
+    bcrypt.hash(req.body.password,saltRounds,(err,hash)=>{
+        const newUser = new User ({
+            email : req.body.username,
+            password:hash
         });
 
+        newUser.save()
+            .then(() => {
+                res.render("secrets.ejs");
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    });
 });
 
 app.post("/login", (req, res) => {
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({ email: username }).exec()
-        .then((findUser) => {
-            if (findUser) {
-                // Assuming you have a 'password' field in your userSchema
-                if (findUser.password === password) {
-                    res.render("secrets.ejs");
-                } else {
-                    res.send("Incorrect password");
-                }
+        .then((foundUser) => {
+            if (foundUser) {
+                
+                bcrypt.compare(password,foundUser.password,(err,result)=>{
+                    
+                    if (result === true) {
+                        
+                        res.render("secrets.ejs");
+                    } else {
+                        res.send("Incorrect password");
+                    }
+                });
             } else {
                 res.send("User not found");
             }
